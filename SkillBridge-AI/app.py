6ab173886d71d0fcb0b6198daca83502fb290555
Santosh_Kumar <san.kumar7363@gmail.com>
2026-07-22 15:05:42 +0530

    app.py

--- SkillBridge-AI/app.py
+++ SkillBridge-AI/app.py
@@ -5,7 +5,7 @@ import requests
 
 app = Flask(__name__)
 
-N8N_WEBHOOK = "http://localhost:5678/webhook/resume-analysis"
+N8N_WEBHOOK = "https://hemi.app.n8n.cloud/webhook/resume-analysis"
 
 UPLOAD_FOLDER = "static/uploads"
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
